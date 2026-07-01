341eb64a5baf3432b7dc279fd6059959e4d1d334
saraylov <saraylov@gmail.com>
2026-07-01 11:53:11 +0300

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/bavarius-next/',
   plugins: [react()],
   server: { port: 3000, open: true }
 })
