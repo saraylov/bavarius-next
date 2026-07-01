6074d0cc31efe017795a63422f31edb062a3472a
saraylov <saraylov@gmail.com>
2026-07-01 12:05:45 +0300

    Fix manifest.json, sw.js and service worker paths for GitHub Pages

--- public/sw.js
+++ public/sw.js
@@ -2,25 +2,25 @@
 const CACHE = 'bavarius-v1'
 
 const PRECACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/img/bav_min.png',
-  '/img/logo.png',
-  '/img/hamburger_icon.png',
-  '/img/business.svg',
-  '/img/pin.svg',
-  '/img/icon_vk.png',
-  '/img/bg_new.png',
-  '/img/bg_main.jpg',
-  '/img/bg_main2.jpg',
-  '/img/bg_main3.jpg',
-  '/img/bavarius_prev.jpg',
-  '/img/icon-192.png',
-  '/img/icon-512.png',
-  '/fonts/CervoRegular.woff',
-  '/fonts/CervoMedium.woff',
-  '/fonts/RobotoRegular.woff',
+  '/bavarius-next/',
+  '/bavarius-next/index.html',
+  '/bavarius-next/manifest.json',
+  '/bavarius-next/img/bav_min.png',
+  '/bavarius-next/img/logo.png',
+  '/bavarius-next/img/hamburger_icon.png',
+  '/bavarius-next/img/business.svg',
+  '/bavarius-next/img/pin.svg',
+  '/bavarius-next/img/icon_vk.png',
+  '/bavarius-next/img/bg_new.png',
+  '/bavarius-next/img/bg_main.jpg',
+  '/bavarius-next/img/bg_main2.jpg',
+  '/bavarius-next/img/bg_main3.jpg',
+  '/bavarius-next/img/bavarius_prev.jpg',
+  '/bavarius-next/img/icon-192.png',
+  '/bavarius-next/img/icon-512.png',
+  '/bavarius-next/fonts/CervoRegular.woff',
+  '/bavarius-next/fonts/CervoMedium.woff',
+  '/bavarius-next/fonts/RobotoRegular.woff',
 ]
 
 self.addEventListener('install', event => {
@@ -51,7 +51,7 @@ self.addEventListener('fetch', event => {
           caches.open(CACHE).then(cache => cache.put(event.request, clone))
           return response
         })
-        .catch(() => caches.match(event.request).then(r => r || caches.match('/')))
+        .catch(() => caches.match(event.request).then(r => r || caches.match('/bavarius-next/')))
     )
   } else {
     event.respondWith(
--- src/main.jsx
+++ src/main.jsx
@@ -14,5 +14,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
 // Register PWA Service Worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').catch(() => {})
+  navigator.serviceWorker.register(import.meta.env.BASE_URL + 'sw.js').catch(() => {})
 }
